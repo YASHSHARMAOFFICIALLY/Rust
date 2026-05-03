6ccd29c415c2609531806f4577f34432d31f0e13
Yash sharma <yashsharmaofficially@gmail.com>
2026-05-03 23:30:34 +0530

    update

--- day01/calculator/src/main.rs
+++ day01/calculator/src/main.rs
@@ -1,52 +1,68 @@
 use std::io;
 fn main(){
-    println!("Welcome to the calculator");
+    println!("Welcome to Calulator");
 
     loop {
-
-        loop{
+        let num1 = loop{
             println!("Enter the first number");
-            let mut num1 = String::new();
-                io::stdin()
-                .read_line(&mut num1)
-                .expect("failed to read input");
-            let num1:i32 = match num1.trim().parse() {
+            let mut input = String::new();
+                io::stdin().read_line(&mut input).expect("Failed to read line");
+            match input.trim().parse::<i32>(){
                 Ok(num)=>break num,
                 Err(_)=>{
-                    println!("please enter valid number");
+                    println!("Invalid Number,try again");
                     continue;
                 }
-            };
+            }
         };
-        loop {
-            println!("Enter the second number");
-            let mut num2 = String::new();
-            io::stdin()
-                .read_line(&mut num2)
-                .expect("Failed to read input");
 
-            let num2:i32 = match num2.trim().parse(){
+        let num2 = loop {
+            println!("Enter the second number");
+            let mut input = String::new();
+             io::stdin().read_line(&mut input).expect("Failed to read line");
+             match input.trim().parse::<i32>(){
                 Ok(num)=>break num,
                 Err(_)=>{
-                    println!("please enter the valid number");
+                    println!("Invalid number,try again");
                     continue;
                 }
-            };
+             }
         };
-        loop {
-            println!("Enter the operator you want");
-            let mut operator = String::new();
-            io::stdin()
-                .read_line(&mut operator)
-                .expect("Failed to read operator");
-            let op:String = match operator.trim(){
-                Ok(op)=>break op,
-                Err(_)=>{
-                    println!("please eneter the valid op");
+
+        let operator = loop {
+            println!("Enter the operator (+,-,*,/,%");
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).expect("Failed to read line");
+
+            let op = input.trim();
+             if op == "+" || op == "-" || op == "*" || op == "/" || op == "%" {
+                break op.to_string();
+            } else {
+                println!("Invalid operator, try again.");
+            }
+        };
+
+        let result = match operator.as_str(){
+              "+" => num1 + num2,
+            "-" => num1 - num2,
+            "*" => num1 * num2,
+            "/" => {
+                if num2 == 0 {
+                    println!("Cannot divide by zero!");
                     continue;
                 }
-            };
+                num1 / num2
+            }
+            "%" => {
+                if num2 == 0 {
+                    println!("Cannot mod by zero!");
+                    continue;
+                }
+                num1 % num2
+            }
+            _ => unreachable!(),
         };
-      
-    };
-}
+        println!("Result is {}",result);
+        break;
+        }
+    }
